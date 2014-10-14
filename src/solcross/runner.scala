dbd46185266c7e500b2dfde88516def0db435bb0
bugkreator <nosuchemail@nosuchdomain.com>
2014-10-14 08:01:17 +0700

    Removed currDepth

--- src/solcross/runner.scala
+++ src/solcross/runner.scala
@@ -3,9 +3,9 @@ package solcross
 object runner extends App {
   val b = Board.initialBoard
   println("****")
-  val startTime :Long = System.currentTimeMillis
+  val startTime: Long = System.currentTimeMillis
   val l = b.getBestMoveList()
   Board.playMoves(b, l).foreach(println)
   val endTime: Long = System.currentTimeMillis()
-  println(endTime-startTime)
+  println(endTime - startTime)
 }
